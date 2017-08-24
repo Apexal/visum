842f802fb345efbf1a3f79cc475fdb8eb686b81d
Frank Matranga <thefrankmatranga@gmail.com>
2017-08-23 22:56:55 -0400

    Hero gradient prefix fixes

--- visum.js
+++ visum.js
@@ -1,7 +1,7 @@
-window.onload = function() {
-  
+window.onload = function () {
+
   const dropdownIcons = document.getElementsByClassName('nav-dropdown-icon');
-  [].forEach.call(dropdownIcons, function(dI) {
+  [].forEach.call(dropdownIcons, function (dI) {
     // Find nav
     dI.onclick = () => {
       const nav = dI.parentElement;
