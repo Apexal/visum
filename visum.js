fd63afa82050e93be1aa1c54ee1ca4081be872ec
Frank Matranga <thefrankmatranga@gmail.com>
2017-08-23 22:05:12 -0400

    Responsive navbar

---
+++ visum.js
@@ -0,0 +1,11 @@
+window.onload = function() {
+  
+  const dropdownIcons = document.getElementsByClassName('nav-dropdown-icon');
+  [].forEach.call(dropdownIcons, function(dI) {
+    // Find nav
+    dI.onclick = () => {
+      const nav = dI.parentElement;
+      nav.classList.toggle('expanded');
+    }
+  });
+}
